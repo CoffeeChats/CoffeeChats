44575f168aad851edede89bb4b3fbc52700c8945
joshbarrientos1 <joshbarrientos1@gmail.com>
2020-08-30 21:14:35 -0400

    database updates

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,22 +8,26 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-      var name = $("input#name").val();
+      var first_name = $("input#first_name").val();
+      var last_name = $("input#last_name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
+
+      //if (firstName.indexOf(' ') >= 0) {
+      //  firstName = name.split(' ').slice(0, -1).join(' ');
+      //}
+
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
         url: "././mail/contact_me.php",
         type: "POST",
         data: {
-          name: name,
+          first_name: first_name,
+          last_name: last_name,
           phone: phone,
           email: email,
           message: message
@@ -46,7 +50,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + first_name + ", it seems that my mail server is not responding. Please try again later!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
+if(empty($_POST['first_name'])      ||
+   empty($_POST['last_name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
@@ -10,15 +10,16 @@ if(empty($_POST['name'])      ||
    return false;
    }
    
-$name = strip_tags(htmlspecialchars($_POST['name']));
+$first_name = strip_tags(htmlspecialchars($_POST['first_name']));
+$last_name = strip_tags(htmlspecialchars($_POST['last_name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'coffeechats.network@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Website Contact Form:  $first_name $last_name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nFirst name: $first_name\n\nLast name: $last_name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "coffeechats.network@gmail.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
