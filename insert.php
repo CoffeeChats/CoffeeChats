2e1ca9e7dba8010cf17b44628786ea6f5c7a8e2b
joshbarrientos1 <joshbarrientos1@gmail.com>
2020-08-31 22:15:41 -0400

    hard code email

--- insert.php
+++ insert.php
@@ -21,7 +21,7 @@
         if ($conn->query($sql) === TRUE) {
             $from = new \SendGrid\Email("Example User", "joshbarrientos1@gmail.com");
             $subject = "Website Contact Form:  $first_name $last_name";
-            $to = new \SendGrid\Email("Example User", $email);
+            $to = new \SendGrid\Email("Example User", "dannybaek5@gmail.com");
             $content = new \SendGrid\Content("text/plain", "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nFirst name: $first_name\n\nLast name: $last_name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message");
             $mail = new \SendGrid\Mail($from, $subject, $to, $content);
             echo "Added: ".$first_name." ".$last_name;
